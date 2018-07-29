60c5c797a520acfe80edf5b0c4488f864e1448c2
chenjue <cccqcn@gmail.com>
2018-07-29 16:50:59 +0800

    add test build task

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 @echo off
 for /f "tokens=1 delims=/" %%i in ("%1") do (
-    echo docker build -t %%i:latest %%i
-    docker build -t %%i:latest -f %1 %%i
+    echo docker build -t %%i:%2 -f %1 %%i
+    docker build -t %%i:%2 -f %1 %%i
 )
