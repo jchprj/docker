1fa1e7ec58f54e6eb6ec4ccf00a16340e3455a97
chenjue <cccqcn@gmail.com>
2018-07-31 02:12:51 +0800

    add build image task using filename as tag

--- build.cmd
+++ build.cmd
@@ -1,5 +1,31 @@
 @echo off
-for /f "tokens=1 delims=/" %%i in ("%1") do (
-    echo docker build -t %%i:%2 -f %1 %%i
-    docker build -t %%i:%2 -f %1 %%i
+set buildTag=latest
+set imageName=
+if "%2"=="1" goto build1
+if "%2"=="2" goto build2
+if Not "%2"=="" (
+    set buildTag=%2 
+    goto build1
 )
+goto exit
+
+:build1
+for /f "tokens=1 delims=/" %%i in ("%1") do (
+    set imageName=%%i
+    goto build
+)
+
+:build2
+for /f "tokens=1-2 delims=/" %%i in ("%1") do (
+    for /f "tokens=1 delims=." %%n in ("%%j") do (
+        set buildTag=%%n
+        set imageName=%%i
+        goto build
+    )
+)
+
+:build
+echo docker build -t %imageName%:%buildTag% -f %1 %imageName%
+REM docker build -t %%i:%buildTag% -f %1 %%i
+
+:exit
