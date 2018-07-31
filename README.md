8e5757253a73b123553708b0c7ca61c1a2721ddb
chenjue <chenjue@stevegame.cn>
2018-07-31 23:16:14 +0800

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 docker local test
+
+# build.cmd
+
+Windows only, it is designed for use with VS Code task
+
+## Parameters
+
+build.cmd \<relative path dockerfile\> \<build tag\>
+
+If *build tag* is ommited, build using filename as tag
+
+## Examples
+
+`build.cmd test-alpine\alias.Dockerfile`  
+Equals to:  
+`docker build -t test-alpine:alias -f test-alpine/alias.Dockerfile test-alpine`
+
+`build.cmd test-alpine\alias.Dockerfile latest`  
+Equals to:  
+`docker build -t test-alpine:latest -f test-alpine/alias.Dockerfile test-alpine`
+
+## VS Code task configuration
+
+```
+{
+	"label": "build image with tag",
+	"type": "shell",
+	"command": "cmd",
+	"args": [
+		"/C",
+		"${workspaceFolder}/build.cmd",
+		"${relativeFile}"
+	],
+	"problemMatcher": []
+}
+```
+
+# build.sh 
+
+Not available yet
--- build.cmd
+++ build.cmd
@@ -1,24 +1,11 @@
 @echo off
-set buildTag=latest
-set imageName=
-if "%2"=="1" goto build1
-if "%2"=="2" goto build2
-if Not "%2"=="" (
-    set buildTag=%2 
-    goto build1
-)
-goto exit
-
-:build1
-for /f "tokens=1 delims=/" %%i in ("%1") do (
-    set imageName=%%i
-    goto build
-)
-
-:build2
 for /f "tokens=1-2 delims=/" %%i in ("%1") do (
     for /f "tokens=1 delims=." %%n in ("%%j") do (
-        set buildTag=%%n
+        if Not "%2"=="" (
+            set buildTag=%2 
+        ) else (
+            set buildTag=%%n
+        )
         set imageName=%%i
         goto build
     )
@@ -26,6 +13,4 @@ for /f "tokens=1-2 delims=/" %%i in ("%1") do (
 
 :build
 echo docker build -t %imageName%:%buildTag% -f %1 %imageName%
-REM docker build -t %%i:%buildTag% -f %1 %%i
-
-:exit
+docker build -t %imageName%:%buildTag% -f %1 %imageName%
