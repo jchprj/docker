da1973c881c101f05887aa9364ff94dc847345c5
chenjue <cccqcn@gmail.com>
2018-08-18 18:28:21 +0800

    add echo to remove cmd

--- build.cmd
+++ build.cmd
@@ -14,6 +14,7 @@ for /f "tokens=1-2 delims=/" %%i in ("%1") do (
 )
 
 :build
+echo docker image remove %imageName%:%buildTag%
 docker image remove %imageName%:%buildTag%
 echo docker build -t %imageName%:%buildTag% -f %1 %imageName%
 docker build -t %imageName%:%buildTag% -f %1 %imageName%
